b6aa537b07badac02b79626ca4ce45a33b6771f0
Christoph Heiss <christoph@c8h4.io>
2024-03-14 16:58:55 +0100

    vimPlugins.none-ls-shellcheck-nvim: inint at 2024-02-28
    Signed-off-by: Christoph Heiss <christoph@c8h4.io>

--- pkgs/applications/editors/vim/plugins/generated.nix
+++ pkgs/applications/editors/vim/plugins/generated.nix
@@ -7019,6 +7019,18 @@ final: prev:
     meta.homepage = "https://github.com/gbprod/none-ls-luacheck.nvim/";
   };
 
+  none-ls-shellcheck-nvim = buildVimPlugin {
+    pname = "none-ls-shellcheck.nvim";
+    version = "2024-02-28";
+    src = fetchFromGitHub {
+      owner = "gbprod";
+      repo = "none-ls-shellcheck.nvim";
+      rev = "1eed283a7ede771b522a0a9f30bb604f02f51d64";
+      sha256 = "1hs0q9a0xwyqml0bfmplk89f1dk4nyg6aapfarnx44zqiw1183kn";
+    };
+    meta.homepage = "https://github.com/gbprod/none-ls-shellcheck.nvim/";
+  };
+
   none-ls-nvim = buildVimPlugin {
     pname = "none-ls.nvim";
     version = "2024-02-29";
