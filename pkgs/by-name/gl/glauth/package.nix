3b98ccbc28e6efcc4e950c2e00e0f26f8ea4c2b5
Christoph Heiss <christoph@c8h4.io>
2025-05-14 11:37:01 +0200

    wip! glauth: 2.3.2 -> 2.4.0
    Signed-off-by: Christoph Heiss <christoph@c8h4.io>

--- pkgs/by-name/gl/glauth/package.nix
+++ pkgs/by-name/gl/glauth/package.nix
@@ -4,46 +4,65 @@
   buildGoModule,
   oath-toolkit,
   openldap,
+  versionCheckHook,
+  tree,
 }:
 
 buildGoModule rec {
   pname = "glauth";
-  version = "2.3.2";
+  version = "2.4.0";
 
   src = fetchFromGitHub {
     owner = "glauth";
     repo = "glauth";
     rev = "v${version}";
-    hash = "sha256-FOhtL8nIm5kuKRxFtkrDyUU2z1K22ZdHaes3GY0KmfQ=";
+    hash = "sha256-UUTL+ZnHRSYuD/TUYpsuo+Nu90kpA8ZL4XaGz6in3ME=";
   };
 
-  vendorHash = "sha256-MfauZRufl3kxr1fqatxTmiIvLJ+5JhbpSnbTHiujME8=";
+  vendorHash = "sha256-VnhKdOKWrXadC+nhUqkhNX8L61IYYD9TVPEsZwSP2YM=";
+
+  postPatch = ''
+    mkdir -p v2/pkg/embed
+    echo 'package embed' > v2/pkg/embed/sqlite.go
+    echo 'github.com/davecgh/go-spew v1.1.0/go.mod h1:J7Y8YcW2NihsgmVo/mv3lAwl/skON4iLHjSsI+c5H38=' >> v2/go.sum
+
+    substituteInPlace v2/internal/version/const.go \
+      --replace-fail 'Version = "v2.3.1"' 'Version = "v${version}"'
+  '';
 
   nativeCheckInputs = [
     oath-toolkit
     openldap
   ];
 
+  strictDeps = true;
+
   modRoot = "v2";
+
+  tags = "noembed";
 
   # Disable go workspaces to fix build.
   env.GOWORK = "off";
 
-  # Based on ldflags in <glauth>/Makefile.
+  # Based on ldflags in v2/Makefile
   ldflags = [
     "-s"
     "-w"
-    "-X main.GitClean=1"
-    "-X main.LastGitTag=v${version}"
-    "-X main.GitTagIsCommit=1"
+    "-X"
+    "github.com/glauth/glauth/v2/internal/version.Version=${version}"
   ];
 
   # Tests fail in the sandbox.
   doCheck = false;
 
+  doInstallCheck = true;
+  nativeInstallCheckInputs = [ versionCheckHook ];
+  versionCheckProgramArg = "--version";
+
   meta = with lib; {
     description = "Lightweight LDAP server for development, home use, or CI";
     homepage = "https://github.com/glauth/glauth";
+    changelog = "https://github.com/curl/trurl/releases/tag/v${version}";
     license = licenses.mit;
     maintainers = with maintainers; [
       bjornfor
